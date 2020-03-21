5640cd2fe509c5fd6e57ab6b369989aeedbb35b1
minxuezh <55081293+minxuezh@users.noreply.github.com>
2020-03-22 01:17:39 +0800

    Add files via upload

---
+++ Week 3/movie.py
@@ -0,0 +1,7 @@
+# 將電影的片長（以分鐘為單位)、評分、片名儲存為物件並印出
+movie_time = 181
+movie_rating = 8.5
+movie_title = "Avengers: Endgame"
+print(movie_time) 
+print(movie_rating)
+print(movie_title)
---
+++ Week 3/type.py
@@ -0,0 +1,7 @@
+#使用type()函數將電影的片長（以分鐘為單位)、評分、片名這三個物件的類別印出
+movie_time = 181
+movie_rating = 8.5
+movie_title = "Avengers: Endgame"
+print(type(movie_time))
+print(type(movie_rating))
+print(type(movie_title))
