7f7aa025317dd318a9e1813411388e7aa6491fd9
minxuezh <55081293+minxuezh@users.noreply.github.com>
2020-04-28 21:13:01 +0800

    Add files via upload

---
+++ Week 6/身分證尾數.py
@@ -0,0 +1,8 @@
+# 判斷身分證字號的尾數是否為奇數
+id_last_digit = input("請輸入您身分證字號的尾數:")
+id_last_digit = int(id_last_digit)
+print(id_last_digit)
+print(type(id_last_digit))
+modulo = id_last_digit % 2 
+ans= modulo == 1
+print(ans)
