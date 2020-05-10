23b3d4a20eab3920b5df5de8e437034a507389f7
minxuezh <55081293+minxuezh@users.noreply.github.com>
2020-05-10 22:27:57 +0800

    Add files via upload

---
+++ Week 10/奇異博士.py
@@ -0,0 +1,5 @@
+# 隨堂練習: 參考 cast 與 https://www.imdb.com/title/tt4154796/ 將飾演奇異博士（Doctor Strange）的演員用 indexing 方式選出來
+# 索引
+cast = ['Robert Downey Jr.', 'Chris Evans', 'Mark Ruffalo', 'Chris Hemsworth', 'Scarlett Johansson', 'Jeremy Renner', 'Don Cheadle', 'Paul Rudd', 'Benedict Cumberbatch', 'Chadwick Boseman', 'Brie Larson', 'Tom Holland', 'Karen Gillan', 'Zoe Saldana', 'Evangeline Lilly']
+print(cast[-7])
+print(cast[8])
---
+++ Week 10/復仇者聯盟.py
@@ -0,0 +1,5 @@
+# 隨堂練習: 參考 cast 與 https://www.imdb.com/title/tt4154796/ 將第一集復仇者聯盟的六位英雄選出來
+# 切割
+cast = ['Robert Downey Jr.', 'Chris Evans', 'Mark Ruffalo', 'Chris Hemsworth', 'Scarlett Johansson', 'Jeremy Renner', 'Don Cheadle', 'Paul Rudd', 'Benedict Cumberbatch', 'Chadwick Boseman', 'Brie Larson', 'Tom Holland', 'Karen Gillan', 'Zoe Saldana', 'Evangeline Lilly']
+print(len(cast))
+print(cast[:6])
